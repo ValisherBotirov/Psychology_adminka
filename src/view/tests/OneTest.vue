edd7f340a5488f2008b66b4ddff2f3510780136a
Ikromov-fs <nodirikromov10@gmail.com>
2023-08-18 18:10:30 +0500

    style

--- src/view/tests/OneTest.vue
+++ src/view/tests/OneTest.vue
@@ -1,226 +1,229 @@
 <template>
-    <div class="transition duration-500">
-        <div class="border border-gray-600 py-4 px-5 bg-white">
-            <div class="flex items-center gap-4 mt-1 mb-4" v-if="!routeId">
-                <SingleSelect
-                        v-model="form.testID"
-                        placeholder="Test nomini tanlang"
-                        :data="subcategoryData"
-                        :error="$v.testID.$error"
-                        class="w-full"
-                />
-            </div>
-            <div class="flex gap-6">
-        <Textarea
-                custom-class="h-[90px]"
-                label="Savolni matnini kiriting"
-                class="w-full"
-                v-model="form.title"
+  <div class="transition duration-500">
+    <div class="border border-gray-600 py-4 px-5 bg-white">
+      <div class="flex items-center gap-4 mt-1 mb-4" v-if="!routeId">
+        <SingleSelect
+          v-model="form.testID"
+          placeholder="Test nomini tanlang"
+          :data="subcategoryData"
+          :error="$v.testID.$error"
+          class="w-full"
         />
-                <UploadImages
-                        line
-                        class="w-full"
-                        label="Savol rasmini yuklash (ixtiyoriy)"
-                        @getBase64="getQuestionImages"
-                />
-            </div>
-            <div class="flex items-end gap-10 my-4">
-                <div
-                        class="flex gap-2 cursor-pointer group mb-2 w-full"
-                        @click="addNewOption"
-                >
-                    <div
-                            class="w-4 h-4 border border-blue-600 flex justify-center items-center rounded-[50%] cursor-pointer p-3"
-                    >
-                        <i class="fa-solid fa-plus text-red-400"></i>
-                    </div>
-                    <p
-                            class="text-black transition group-hover:text-blue-700 select-none"
-                    >
-                        Yangi test varianti qo'shish
-                    </p>
-                </div>
-                <FormInput
-                        placeholder="0"
-                        custom-class="py-2"
-                        label="Savol uchun ball belgilang"
-                        class="w-full"
-                        :error="$v.score.$error"
-                        v-model="form.score"
-                />
-            </div>
-            <div class="grid grid-cols-2 gap-x-6 gap-y-3">
-                <div
-                        class="flex gap-4 items-center"
-                        v-for="(item, index) in form.answerCreateDTOList"
-                        :key="index"
-                >
-                    <div class="flex flex-col gap-2 items-center">
-                        <input
-                                id="teal-radio"
-                                type="radio"
-                                :value="item.id"
-                                v-model="form.correct"
-                                name="colored-radio"
-                                class="w-5 h-5 cursor-pointer"
-                        />
-                        <i
-                                class="fa-solid fa-trash text-lg text-red-600 cursor-pointer"
-                                @click="deleteOption(item.id)"
-                        ></i>
-                    </div>
-                    <div class="w-full flex flex-col gap-2">
-            <Textarea
-                    custom-class="h-[60px] w-full"
-                    placeholder="Varinat"
-                    v-model="item.optionText"
-            />
-                        <UploadImages
-                                small
-                                @getBase64="e => fetchUploadImagesId(item,e)"
-                                :inputId="`file${item.id}`"
-                                :img="item.image"
-                        />
-                    </div>
-                </div>
-            </div>
-            <div class="flex justify-end mt-4">
-                <SButton variant="secondary" class="w-[250px]" @click="onSubmit"
-                >Saqlash
-                </SButton
-                >
-            </div>
+      </div>
+      <div class="flex gap-6">
+        <Textarea
+          custom-class="h-[90px]"
+          label="Savolni matnini kiriting"
+          class="w-full"
+          v-model="form.title"
+        />
+        <UploadImages
+          line
+          class="w-full"
+          label="Savol rasmini yuklash (ixtiyoriy)"
+          @getBase64="getQuestionImages"
+        />
+      </div>
+      <div class="flex items-end gap-10 my-4">
+        <div
+          class="flex gap-2 cursor-pointer group mb-2 w-full"
+          @click="addNewOption"
+        >
+          <div
+            class="w-4 h-4 border border-blue-600 flex justify-center items-center rounded-[50%] cursor-pointer p-3"
+          >
+            <i class="fa-solid fa-plus text-red-400"></i>
+          </div>
+          <p
+            class="text-black transition group-hover:text-blue-700 select-none"
+          >
+            Yangi test varianti qo'shish
+          </p>
         </div>
+        <FormInput
+          placeholder="0"
+          custom-class="py-2"
+          label="Savol uchun ball belgilang"
+          class="w-full"
+          :error="$v.score.$error"
+          v-model="form.score"
+        />
+      </div>
+      <div class="grid grid-cols-2 gap-x-6 gap-y-3">
+        <div
+          class="flex gap-4 items-center"
+          v-for="(item, index) in form.answerCreateDTOList"
+          :key="index"
+        >
+          <div class="flex flex-col gap-2 items-center">
+            <input
+              id="teal-radio"
+              type="radio"
+              :value="item.id"
+              v-model="form.correct"
+              name="colored-radio"
+              class="w-5 h-5 cursor-pointer"
+            />
+            <i
+              class="fa-solid fa-trash text-lg text-red-600 cursor-pointer"
+              @click="deleteOption(item.id)"
+            ></i>
+          </div>
+          <div class="w-full flex flex-col gap-2">
+            <Textarea
+              custom-class="h-[60px] w-full"
+              placeholder="Varinat"
+              v-model="item.optionText"
+            />
+            <UploadImages
+              small
+              @getBase64="(e) => fetchUploadImagesId(item, e)"
+              :inputId="`file${item.id}`"
+              :img="item.image"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-end mt-4">
+        <SButton variant="secondary" class="w-[250px]" @click="onSubmit"
+          >Saqlash
+        </SButton>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import SButton from "@/components/buttons/SButton.vue";
 import Textarea from "@/components/input/textarea.vue";
 import UploadImages from "@/components/input/uploadImages.vue";
-import {onMounted, ref} from "@vue/runtime-core";
-import {useToast} from "vue-toastification";
+import { onMounted, ref } from "@vue/runtime-core";
+import { useToast } from "vue-toastification";
 import FormInput from "@/components/input/FormInput.vue";
-import {computed, reactive} from "vue";
+import { computed, reactive } from "vue";
 import SingleSelect from "@/components/select/SingleSelect.vue";
-import {required} from "@vuelidate/validators";
-import {useVuelidate} from "@vuelidate/core";
-import {useRoute} from "vue-router";
+import { required } from "@vuelidate/validators";
+import { useVuelidate } from "@vuelidate/core";
+import { useRoute } from "vue-router";
 import axios from "../../plugins/axios.js";
-import {useCategoryStore} from "@/store/categories.js";
+import { useCategoryStore } from "@/store/categories.js";
 
-const categoryStore = useCategoryStore()
+const categoryStore = useCategoryStore();
 
 const toast = useToast();
 
 const route = useRoute();
 
-
-const subcategoryData = computed(()=> categoryStore.subCategories.map((el)=>{
+const subcategoryData = computed(() =>
+  categoryStore.subCategories.map((el) => {
     return {
-        value:el.id,
-        label:el.title
-    }
-}));
+      value: el.id,
+      label: el.title,
+    };
+  })
+);
 
 const routeId = route.query.id;
 
 const form = reactive({
-    testType: "CHECKBOX",
-    title: "",
-    imageID: null,
-    testID: "",
-    score: "",
-    correct: null,
-    correctAnswers:[],
-    correctCloseAnswer: "",
-    answerCreateDTOList: [
-        {
-            id: 1,
-            optionText: "salom",
-            image: "",
-            correct: false
-        },
-        {
-            id: 2,
-            optionText: "",
-            image: "",
-            correct: false
-        },
-    ],
+  testType: "CHECKBOX",
+  title: "",
+  imageID: null,
+  testID: "",
+  score: "",
+  correct: null,
+  correctAnswers: [],
+  correctCloseAnswer: "",
+  answerCreateDTOList: [
+    {
+      id: 1,
+      optionText: "salom",
+      image: "",
+      correct: false,
+    },
+    {
+      id: 2,
+      optionText: "",
+      image: "",
+      correct: false,
+    },
+  ],
 });
 
 const rule = computed(() => {
-    return {
-        testID: {required},
-        score:{required}
-    };
+  return {
+    testID: { required },
+    score: { required },
+  };
 });
 
 const $v = useVuelidate(rule, form);
 
 function addNewOption() {
-    const option = {
-        id: form.answerCreateDTOList.length + 1,
-        optionText: "",
-        correct: false,
-        image: "",
-    };
-    form.answerCreateDTOList.push(option);
+  const option = {
+    id: form.answerCreateDTOList.length + 1,
+    optionText: "",
+    correct: false,
+    image: "",
+  };
+  form.answerCreateDTOList.push(option);
 }
 
 function deleteOption(id) {
-    form.answerCreateDTOList = form.answerCreateDTOList.filter(
-        (el) => el.id !== id
-    );
+  form.answerCreateDTOList = form.answerCreateDTOList.filter(
+    (el) => el.id !== id
+  );
 }
 
 // work images input
 
 function getQuestionImages(e) {
-    const formData = new FormData()
-    formData.append("file", e)
-    axios.post('media/upload', formData).then((res) => {
-        console.log(res)
-        form.imageID = res.data.id
-    })
+  const formData = new FormData();
+  formData.append("file", e);
+  axios.post("media/upload", formData).then((res) => {
+    console.log(res);
+    form.imageID = res.data.id;
+  });
 }
 
 // media upload images id
 
-function fetchUploadImagesId(item,e){
-    const formData = new FormData()
-    formData.append("file",e)
-    axios.post('media/upload',formData).then((res)=>{
-        item.image = res.data.id
-    })
+function fetchUploadImagesId(item, e) {
+  const formData = new FormData();
+  formData.append("file", e);
+  axios.post("media/upload", formData).then((res) => {
+    item.image = res.data.id;
+  });
 }
 function onSubmit() {
-    if (!routeId) {
-        $v.value.$validate();
+  if (!routeId) {
+    $v.value.$validate();
+  }
+  if (!$v.value.$error) {
+    form.correctAnswers = [form.correct];
+    console.log(form, "opt");
+    if (!form.correct) {
+      toast.error("Iltimos bitta to'g'ri javobni belgilang");
+    } else {
+      axios
+        .post("/question", form)
+        .then((res) => {
+          console.log(res);
+          toast.success("Test muvaffaqiyatli qo'shildi");
+        })
+        .catch((err) => {
+          toast.error("Qo'shishda xatolik yuz berdi!");
+        })
+        .finally(() => {
+          setTimeout(() => {
+            window.location.reload();
+          }, 1000);
+        });
     }
-    if (!$v.value.$error) {
-        form.correctAnswers = [form.correct]
-        console.log(form, "opt");
-        if (!form.correct) {
-            toast.error("Iltimos bitta to'g'ri javobni belgilang");
-        }
-        else{
-            axios.post('/question',form).then((res)=>{
-                console.log(res)
-                toast.success("Test muvaffaqiyatli qo'shildi")
-            }).catch((err)=>{
-                toast.error("Qo'shishda xatolik yuz berdi!")
-            }).finally(()=>{
-                setTimeout(()=>{
-                    window.location.reload()
-                },1000)
-            })
-        }
-    }
+  }
 }
 
-onMounted(()=>{
-    categoryStore.fetchSubCategoryAll()
-})
+onMounted(() => {
+  categoryStore.fetchSubCategoryAll();
+});
 </script>
