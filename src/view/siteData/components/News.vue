50e0b96d53a89f0804f4aaa1d9090c7153165aab
Ikromov-fs <nodirikromov10@gmail.com>
2023-08-21 19:55:12 +0500

    news

--- src/view/siteData/components/News.vue
+++ src/view/siteData/components/News.vue
@@ -10,20 +10,21 @@
     </div>
     <div class="flex justify-between gap-4 mt-4">
       <FormInput
-        v-model="winnerData.companyName"
-        :error="$v.companyName.$error"
-        label="Hamkor nomi"
-        placeholder="Hamkor nomi"
+        v-model="winnerData.link"
+        type="string"
+        :error="$v.link.$error"
+        label="Url"
+        placeholder="Url"
         class="w-full"
       />
       <UploadImages
         ref="removeImg"
-        :img="winnerData.image"
+        :img="winnerData.imageId"
         @getBase64="imageValu"
         line
         class="w-full"
         label="Rasm yuklash"
-        :error="$v.image.$error"
+        :error="$v.imageId.$error"
       />
     </div>
     <table class="w-full text-sm text-left text-gray-500 mt-4">
@@ -47,25 +48,31 @@
           <th
             class="test-name px-6 py-4 font-medium text-gray-900 whitespace-nowrap max-w-[350px] break-words overflow-x-scroll"
           >
-            {{ item?.name }}
+            {{ item?.link }}
           </th>
           <th class="font-medium text-gray-900 whitespace-nowrap">
             <img
-              :src="item?.image"
+              :src="item?.image?.url"
               alt="image"
-              class="w-[200px] h-[80px] object-fill"
+              class="w-[200px] h-[80px] object-contain"
             />
           </th>
           <td class="flex items-center px-6 py-4 space-x-4 justify-end">
             <div
+              class="font-medium text-blue-600 hover:underline cursor-pointer"
+            >
+              <i class="fa-solid fa-pen-to-square text-[blue] text-[20px]"></i>
+            </div>
+            <div
               class="font-medium text-red-600 hover:underline cursor-pointer"
-              @click="openDeleteModal = true"
+              @click="deleteModal(item?.id)"
             >
               <i class="fa-solid fa-trash text-[red] text-[20px]"></i>
             </div>
             <DeleteModal
               :isOpen="openDeleteModal"
               @closeModal="(e) => (openDeleteModal = e)"
+              @delete="deleted"
             />
           </td>
         </tr>
@@ -75,72 +82,96 @@
 </template>
 <script setup lang="ts">
 import SButton from "@/components/buttons/SButton.vue";
-// import { imageEmits } from "element-plus";
 import DeleteModal from "@/components/modal/DeleteModal.vue";
-
-import { ref, reactive, computed } from "vue";
+import { ref, reactive, computed, onMounted } from "vue";
 import { useToast } from "vue-toastification";
 import FormInput from "@/components/input/FormInput.vue";
 import UploadImages from "@/components/input/uploadImages.vue";
-
 // validator
 import { useVuelidate } from "@vuelidate/core";
 import { required, minLength } from "@vuelidate/validators";
-// import { ShapeFlags } from "@vue/shared";
+import axios from "@/plugins/axios.js";
 
+const toast = useToast();
+const openDeleteModal = ref(false);
+const forDeleteId = ref("");
+const data = ref([]);
 const winnerData = reactive({
-  companyName: "",
-  image: "",
+  link: "",
+  imageId: "",
 });
 
 const rules = computed(() => {
   return {
-    companyName: { required, minLength: minLength(3) },
-    image: { required },
+    link: { required, minLength: minLength(3) },
+    imageId: { required },
   };
 });
 
 function imageValu(e) {
-  winnerData.image = e;
+  const formData = new FormData();
+  formData.append("file", e);
+  axios.post("media/upload", formData).then((res) => {
+    winnerData.imageId = res.data.id;
+  });
 }
 
+// post news api
 const $v = useVuelidate(rules, winnerData);
 const removeImg = ref();
 const addPartnerBtn = async () => {
   $v.value.$validate();
   if (!$v.value.$error) {
     try {
-      // function
+      const newsData = {
+        link: winnerData.link,
+        imageId: winnerData.imageId,
+      };
+      const news = await axios.post("/news", newsData);
+      toast.success("Yangliklar qo'shildi !");
     } catch (error) {
       console.log(error);
     } finally {
-      (winnerData.companyName = ""), (winnerData.image = "");
+      (winnerData.link = ""), (winnerData.imageId = "");
       removeImg.value.removeImage();
       $v.value.$reset();
+      getNews();
     }
   }
 };
 
-const toast = useToast();
-const openDeleteModal = ref(false);
-// test data
-const data = [
-  {
-    id: 1,
-    name: "Nodir Ikromov ",
-    image: "https://i.ytimg.com/vi/YaCiB6JZG9w/sddefault.jpg",
-  },
-  {
-    id: 2,
-    name: "Shahzod Temirov",
-    image: "https://i.ytimg.com/vi/YaCiB6JZG9w/sddefault.jpg",
-  },
-  {
-    id: 1,
-    name: "Nodir Ikromov ",
-    image: "https://i.ytimg.com/vi/YaCiB6JZG9w/sddefault.jpg",
-  },
-];
+// get new api
+async function getNews() {
+  try {
+    const newsGet = await axios.get("/news/get-all");
+    data.value = newsGet.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function deleteModal(item) {
+  openDeleteModal.value = true;
+  forDeleteId.value = item;
+  console.log(item);
+}
+
+// delete news api
+
+async function deleted() {
+  try {
+    const deleteNews = await axios.delete(`news/${forDeleteId.value}`);
+    console.log(deleteNews);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    getNews();
+  }
+}
+
+onMounted(() => {
+  getNews();
+});
 </script>
 <style>
 .test-name::-webkit-scrollbar {
--- src/view/tests/ClosedTest.vue
+++ src/view/tests/ClosedTest.vue
@@ -1,104 +1,139 @@
 <template>
-    <div>
-        <div class="border border-gray-600 py-4 px-5 bg-white">
-            <div class="flex items-center gap-4 mt-1 mb-4" v-if="!routeId">
-                <SingleSelect v-model="form.testID" placeholder="Test nomini tanlang" :data="subcategoryData" :error="$v.testID.$error" class="w-full"/>
-            </div>
-            <div class="flex gap-6">
-                <Textarea custom-class="h-[90px]" label="Savolni matnini kiriting" class="w-full" v-model="form.title"/>
-                <UploadImages  line class="w-full" label="Savol rasmini yuklash (ixtiyoriy)" @getBase64="getQuestionImages" />
-            </div>
-            <div class="flex items-center mt-4 gap-4">
-                 <FormInput placeholder="Javobni kiriting" label="Yoziladigan javobni kiriting"  custom-class="py-[10px]" class=" w-full" v-model="form.correctCloseAnswer" :error="$v.correctCloseAnswer.$error"/>
-                <FormInput placeholder="0" custom-class="py-[10px]" label="Savol uchun ball belgilang" class="w-full" v-model="form.score" :error="$v.score.$error"/>
-            </div>
-            <div class="flex justify-end mt-6 ">
-                <SButton variant="secondary" class="w-[250px]" @click="onSubmit">Saqlash</SButton>
-            </div>
-        </div>
+  <div>
+    <div class="border border-gray-600 py-4 px-5 bg-white">
+      <div class="flex items-center gap-4 mt-1 mb-4" v-if="!routeId">
+        <SingleSelect
+          v-model="form.testID"
+          placeholder="Test nomini tanlang"
+          :data="subcategoryData"
+          :error="$v.testID.$error"
+          class="w-full"
+        />
+      </div>
+      <div class="flex gap-6">
+        <Textarea
+          custom-class="h-[90px]"
+          label="Savolni matnini kiriting"
+          class="w-full"
+          v-model="form.title"
+        />
+        <UploadImages
+          line
+          class="w-full"
+          label="Savol rasmini yuklash (ixtiyoriy)"
+          @getBase64="getQuestionImages"
+        />
+      </div>
+      <div class="flex items-center mt-4 gap-4">
+        <FormInput
+          placeholder="Javobni kiriting"
+          label="Yoziladigan javobni kiriting"
+          custom-class="py-[10px]"
+          class="w-full"
+          v-model="form.correctCloseAnswer"
+          :error="$v.correctCloseAnswer.$error"
+        />
+        <FormInput
+          placeholder="0"
+          custom-class="py-[10px]"
+          label="Savol uchun ball belgilang"
+          class="w-full"
+          v-model="form.score"
+          :error="$v.score.$error"
+        />
+      </div>
+      <div class="flex justify-end mt-6">
+        <SButton variant="secondary" class="w-[250px]" @click="onSubmit"
+          >Saqlash</SButton
+        >
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import SButton from "@/components/buttons/SButton.vue";
 import Textarea from "@/components/input/textarea.vue";
 import UploadImages from "@/components/input/uploadImages.vue";
-import {onMounted, ref} from "@vue/runtime-core";
-import {useToast} from "vue-toastification";
+import { onMounted, ref } from "@vue/runtime-core";
+import { useToast } from "vue-toastification";
 import FormInput from "@/components/input/FormInput.vue";
-import {computed, reactive} from "vue";
-import {required} from "@vuelidate/validators";
-import {useVuelidate} from "@vuelidate/core";
+import { computed, reactive } from "vue";
+import { required } from "@vuelidate/validators";
+import { useVuelidate } from "@vuelidate/core";
 import SingleSelect from "@/components/select/SingleSelect.vue";
-import {useRoute} from "vue-router";
-import {useCategoryStore} from "@/store/categories.js";
+import { useRoute } from "vue-router";
+import { useCategoryStore } from "@/store/categories.js";
 import axios from "../../plugins/axios.js";
 
-const categoryStore = useCategoryStore()
+const categoryStore = useCategoryStore();
 
-const toast = useToast()
-const route = useRoute()
+const toast = useToast();
+const route = useRoute();
 
-
-
-const subcategoryData = computed(()=> categoryStore.subCategories.map((el)=>{
+const subcategoryData = computed(() =>
+  categoryStore.subCategories.map((el) => {
     return {
-        value:el.id,
-        label:el.title
-    }
-}));
+      value: el.id,
+      label: el.title,
+    };
+  })
+);
 
-const routeId = route.query.id
+const routeId = route.query.id;
 
 const form = reactive({
-    testType:'CLOSE_QUESTIONS',
-    title:"",
-    imageID:"",
-    testID:"",
-    correctCloseAnswer:"",
-    score:null
-})
+  testType: "CLOSE_QUESTIONS",
+  title: "",
+  imageID: "",
+  testID: "",
+  correctCloseAnswer: "",
+  score: null,
+});
 
-const rule = computed(()=> {
-    return{
-        correctCloseAnswer:{required},
-        score:{required},
-        testID:{required}
-    }
-})
+const rule = computed(() => {
+  return {
+    correctCloseAnswer: { required },
+    score: { required },
+    testID: { required },
+  };
+});
 
-const $v = useVuelidate(rule,form)
+const $v = useVuelidate(rule, form);
 
 function getQuestionImages(e) {
-    const formData = new FormData()
-    formData.append("file", e)
-    axios.post('media/upload', formData).then((res) => {
-        console.log(res)
-        form.imageID = res.data.id
-    })
+  const formData = new FormData();
+  formData.append("file", e);
+  axios.post("media/upload", formData).then((res) => {
+    console.log(res);
+    form.imageID = res.data.id;
+  });
 }
 
-function onSubmit(){
-    if(!routeId){
-        $v.value.$validate()
-    }
-    if(!$v.value.$error){
-        console.log(form,"opt")
-        axios.post('/question',form).then((res)=>{
-            console.log(res)
-            toast.success("Test muvaffaqiyatli qo'shildi")
-        }).catch((err)=>{
-            toast.error("Qo'shishda xatolik yuz berdi!")
-        }).finally(()=>{
-            setTimeout(()=>{
-                window.location.reload()
-            },1000)
-        })
-    }
+function onSubmit() {
+  if (!routeId) {
+    $v.value.$validate();
+  }
+  if (!$v.value.$error) {
+    console.log(form, "opt");
+    axios
+      .post("/question", form)
+      .then((res) => {
+        console.log(res);
+        toast.success("Test muvaffaqiyatli qo'shildi");
+      })
+      .catch((err) => {
+        toast.error("Qo'shishda xatolik yuz berdi!");
+      })
+      .finally(() => {
+        setTimeout(() => {
+          window.location.reload();
+        }, 1000);
+      });
+  }
 }
 
-onMounted(()=>{
-    categoryStore.fetchSubCategoryAll()
-})
-
+onMounted(() => {
+  categoryStore.fetchSubCategoryAll();
+});
 </script>
